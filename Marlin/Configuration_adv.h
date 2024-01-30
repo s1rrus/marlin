4896b45367638f502d67a769cc2a175e15083955
Mikhail Bulgakov <mikhail.bulgakov@icloud.com>
2024-01-29 16:21:02 -0800

    Increase hybrid threshold to 200 mm/s

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3105,11 +3105,11 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
+  #define X_HYBRID_THRESHOLD     200  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
+  #define Y_HYBRID_THRESHOLD     200
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
+  #define Z_HYBRID_THRESHOLD       6
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
